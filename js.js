4320a3f662c6c7e952c3201b025bb21fe5ff5ba3
Astrocat2374 <22ignacio.luke@waipahuh.k12.hi.us>
2020-02-07 10:47:00 -1000

    commit

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
-//Calculate Tip
+
+ document.body.style.backgroundImage="url('https://steamuserimages-a.akamaihd.net/ugc/868489934988939865/834D5AD4F757440528E787627FB9063DE163E194/')"
+//Math stuff's
 function calculateTip() {
   var billAmt = document.getElementById("billamt").value;
   var serviceQual = document.getElementById("serviceQual").value;
@@ -10,18 +12,24 @@ function calculateTip() {
   }
   if (billAmt === "uwu") {
     alert("Okay, I know this is a really bad idea but I'm already here so here we fuckin go,rawr x3 nuzzles, pounces on you, uwu you so warm (Ooh) couldn't help but notice your bulge from across the floor nuzzles your necky wecky-tilde murr-tilde, hehe unzips your baggy ass pants, oof baby you so musky.Take me home, pet me, and make me yours and don't forget to stuff me.see me wag my widdle baby tail all for your buldgy-wuldgy. Kissies and lickies your neck.I hope daddy likies. Nuzzles and wuzzles your chest (Yuh).I be (Yeah) gettin thirsty.Hey, I got a little itch, you think you can help me?. Only seven inches long, uwu, please adopt me. Paws on your buldge as I lick my lips (UwU, punish me please).'Bout to hit 'em with this furry shit (He don't see it comin')");
+    document.body.style.backgroundImage="url('https://thumbs.gfycat.com/YearlyImpressiveIndianpalmsquirrel-size_restricted.gif')";
     return;
   }
-  if (billAmt === "what is the sun"){
-    alert('https://www.youtube.com/watch?v=KdbI8BwrA1o');
+  if (billAmt === "sun"){
+    document.body.style.backgroundImage="url('https://i.ytimg.com/vi/O0_9ZpcGkJc/maxresdefault.jpg')";
     return;
   }
-  //validate input
+  if (billAmt === "pencil"){
+    alert("(Background) One of the oldest and most widely used writing utensils, the pencil originated in pre-historic times when chalky rocks and charred sticks were used to draw on surfaces as varied as animal hides and cave walls. The Greeks and Romans used flat pieces of lead to draw faint lines on papyrus, but it was not until the late 1400s that the earliest direct ancestor of today's pencil was developed. About one hundred years later graphite, a common mineral occurring as soft, lustrous veins in rocks, was discovered near Borrowdale in northwestern England. The Borrowdale mine supplied Europe with graphite for several hundred years; however, because people could not then differentiate between graphite and lead, they referred to the former as black lead. Cut into rods or strips, graphite was heavily wrapped in twine to provide strength and a comfortable handle. The finished product, called a lead pencil, was quite popular. In the late sixteenth century, a method for gluing strips of wood around graphite was discovered in Germany, and the modern pencil began to take form. In 1779, scientists determined that the material they had previously thought was lead was actually a form of microcrystalline carbon that they named graphite (from the Greek 'graphein' meaning 'to write'). Graphite is one of the three natural forms of pure carbon—the others are coal and diamond.)");
+    return;
+  }
+
+  //make sure imput makes sense(check)
   if (billAmt === "" || serviceQual == 0) {
     alert("You forgot something");
     return;
   }
-  //Check to see if this input is empty or less than or equal to 1
+  //checks if imput of people is greater than zero(check)
   if (numOfPeople === "" || numOfPeople < 1) {
     numOfPeople = 1;
     document.getElementById("each").style.display = "none";
@@ -29,23 +37,23 @@ function calculateTip() {
     document.getElementById("each").style.display = "block";
   }
 
-  //Calculate tip
+  //calculate tip(variable)
   var total = (billAmt * serviceQual) / numOfPeople;
-  //round to two decimal places
+  //round to two decimal places(display/variable)
   total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
+  //next line allows us to always have two digits after decimal point(display)
   total = total.toFixed(2);
-  //Display the tip
+  //display the tip(display)
   document.getElementById("totalTip").style.display = "block";
   document.getElementById("tip").innerHTML = total;
 
 }
 
-//Hide the tip amount on load
+//Hide the tip amount on load(dont remember why the tutorial had me write this)
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-//click to call function
+//click to calculate(display)
 document.getElementById("calculate").onclick = function() {
   calculateTip();
 
